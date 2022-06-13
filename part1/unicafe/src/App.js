c86020e132697de5c5c39d1ce5a34385b877042a
stephdolgo <stephdolgo@gmail.com>
2022-06-13 19:43:06 -0400

    Exercises 1.7 - 1.14 complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,30 +1,67 @@
 import { useState } from 'react'
 
+const Btn = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+    </>
+  )
+}
+
+const Statistics = ({good, neutral, bad} ) => {
+  const all = good + neutral + bad
+  const sum = good + 0 - bad
+  const avg = sum / all
+  const positiveAvg = (good / all) * 100 + "%"
+
+  if (all === 0) {
+    return (
+
+        <p>No feedback given</p>
+
+    )
+  } else {
+    return (
+      <div>
+        <StatisticLine text="Good:" value={good}/>
+        <StatisticLine text="Neutral:" value={neutral}/>
+        <StatisticLine text="Bad:" value={bad}/>
+        <StatisticLine text="All:" value={all}/>
+        <StatisticLine text="Average:" value={avg}/>
+        <StatisticLine text="Positive:" value={positiveAvg}/>
+      </div>
+    )
+  }
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const counter = (rating, setRating) => setRating(rating + 1)
+
   return (
-    <div>
+    <>
       <h1>Give Feedback</h1>
-      <button onClick={() => setGood(good + 1)}>
-        Good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-        Neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        Bad
-      </button>
-      <h2>Statistics</h2>
       <div>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
+        <Btn handleClick={() => counter(good, setGood)} text={"good"} />
+        <Btn handleClick={() => counter(neutral, setNeutral)} text={"neutral"} />
+        <Btn handleClick={() => counter(bad, setBad)} text={"bad"} />
       </div>
-    </div>
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </>
   )
 }
 
